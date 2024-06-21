d14fbcd179a1b5fd05a15f2defd546e490b6c55a
Andrii Kviatkovskyi <133859288+AndriiKviatkovskyi@users.noreply.github.com>
2024-06-21 15:19:45 +0300

    added messi

--- src/main/java/org/kviat/Main.java
+++ src/main/java/org/kviat/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
         FootballPlayer player = new FootballPlayer("Іван", "Петренко", 25, "Нападник");
 
+        FootballPlayer player2 = new FootballPlayer("Ліонель", "Мессі", 36, "Нападник");
+
         System.out.println(player.toString());
+
+        System.out.println(player2.toString());
     }
 }
