b822b8d5cf2f9fd71bfb7289165d227d4d76855e
Andrii Kviatkovskyi <133859288+AndriiKviatkovskyi@users.noreply.github.com>
2024-06-21 15:31:22 +0300

    Update Main.java

--- src/main/java/org/kviat/Main.java
+++ src/main/java/org/kviat/Main.java
@@ -3,7 +3,7 @@ package org.kviat;
 public class Main {
     public static void main(String[] args) {
 
-        FootballPlayer player = new FootballPlayer("Іван", "Петренко", 25, "Нападник");
+        FootballPlayer player = new FootballPlayer("Іван", "Іваненко", 25, "Нападник");
 
         FootballPlayer player2 = new FootballPlayer("Ліонель", "Мессі", 36, "Нападник");
 
