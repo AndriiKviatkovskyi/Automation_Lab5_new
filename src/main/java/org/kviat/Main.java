8e6f0f736dee9a2ac3d827a02c3773be03bc291b
Andrii Kviatkovskyi <133859288+AndriiKviatkovskyi@users.noreply.github.com>
2024-06-21 15:09:12 +0300

    Initial commit

---
+++ src/main/java/org/kviat/Main.java
@@ -0,0 +1,10 @@
+package org.kviat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        FootballPlayer player = new FootballPlayer("Іван", "Петренко", 25, "Нападник");
+
+        System.out.println(player.toString());
+    }
+}
